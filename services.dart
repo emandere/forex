332f7f1f1e8d909ff31b8afa8131bbc8b68facdc
emandere <emandere@gmail.com>
2016-08-28 22:42:37 -0400

    1. Fix page to allow for trading on mobile screen..Will improve on it later 2. Added logic to check db first before repopulating

--- services.dart
+++ services.dart
@@ -55,21 +55,23 @@ setupDatabase(ForexMongo mongoLayer) async
 
   var result;
   List<String> pairs = ["AUDUSD","EURUSD","GBPUSD","NZDUSD","USDCAD","USDCHF","USDJPY"];
-
-  await mongoLayer.ClearForexValues();
-  await mongoLayer.AddCurrencies(pairs);
-  for(String pair in pairs)
+  List<String> mongoPairs = await mongoLayer.readMongoPairs();
+  if(mongoPairs.isEmpty)
   {
-    List<String> strvalues = new File('data/' + pair + '/' + pair + 'dailyval.txt').readAsLinesSync();
-    List<ForexDailyValue> dailyvals = new List<ForexDailyValue>();
-    for (String line in strvalues)
-    {
-      ForexDailyValue val = new ForexDailyValue.fromString(line, pairName:pair);
-      result = await mongoLayer.addForexDailyValue(val);
-      print(result);
+    await mongoLayer.ClearForexValues();
+    await mongoLayer.AddCurrencies(pairs);
+    for (String pair in pairs) {
+      List<String> strvalues = new File(
+          'data/' + pair + '/' + pair + 'dailyval.txt').readAsLinesSync();
+      List<ForexDailyValue> dailyvals = new List<ForexDailyValue>();
+      for (String line in strvalues) {
+        ForexDailyValue val = new ForexDailyValue.fromString(
+            line, pairName: pair);
+        result = await mongoLayer.addForexDailyValue(val);
+        print(result);
+      }
     }
   }
-
 }
 
 main(List<String> arguments) async
