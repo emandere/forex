1c8d25b5abad06d13897df679e088ec0fcfb0451
emandere <emandere@gmail.com>
2019-03-24 05:15:50 -0400

    updates for new API V3

--- testOanda.dart
+++ testOanda.dart
@@ -4,16 +4,23 @@ import 'package:http/http.dart' as http;
 import 'dart:io';
 main() async
 {
+
+
+
+}
+
+old() async
+{
   var file = new File("keys");
   var authorization = {"Authorization": await file.readAsString()};
   var text = await http.read("https://api-fxtrade.oanda.com/labs/v1/historical_position_ratios?instrument=EUR_USD&period=86400",
-  headers:authorization);
+      headers:authorization);
   print(text);
 
 
   var fileAccount = new File("account");
   var accountId = await fileAccount.readAsString();
-  var url =  "https://api-fxtrade.oanda.com/v1/accounts/$accountId/orders";
+  var url =  "https://api-fxtrade.oanda.com/v3/accounts/$accountId/orders";
   var bodyMap = {};
 
   bodyMap["instrument"]="AUD_USD";
@@ -26,5 +33,42 @@ main() async
   print("Response status: ${response.statusCode}");
   print("Response body: ${response.body}");
 
+}
+
+newcall() async
+{
+  var file = new File("keys");
+  var authorization = {"Authorization": await file.readAsString()};
+  //var text = await http.read("https://api-fxtrade.oanda.com/labs/v1/historical_position_ratios?instrument=EUR_USD&period=86400",
+  //    headers:authorization);
+  //print(text);
+
+
+  var fileAccount = new File("account");
+  var accountId = await fileAccount.readAsString();
+  var url =  "https://api-fxtrade.oanda.com/v3/accounts/$accountId/orders";
+  //Take note of the -2 for short
+  var order =
+            {
+                "order":
+                {
+                    "units":"-2",
+                    "instrument": "EUR_USD",
+                    "timeInForce": "FOK",
+                    "type": "MARKET",
+                    "positionFill": "DEFAULT",
+                    "stopLossOnFill":
+                    {
+                      "price": "1.14530"
+                    },
+                    "takeProfitOnFill":
+                    {
+                      "price": "1.14530"
+                    }
+                }
+            };
+  var response = await http.post(url,body:order,headers:authorization);
+  print("Response status: ${response.statusCode}");
+  print("Response body: ${response.body}");
 
 }
